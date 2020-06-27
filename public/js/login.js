7ad3b3d66c3f5c67bb659feec5da84b212ee3ab7
Ítalo de Queiroz <italo.de.queiroz@avanade.com>
2020-06-27 15:16:38 -0300

    Criando aplicação

---
+++ public/js/login.js
@@ -0,0 +1,35 @@
+function uiConfig(){
+    return{
+        signInFlow: 'popup',
+        signInSuccessUrl: '#',
+        signInOptions: [
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+          ]
+    }
+}
+
+function logar(){
+    var ui = firebaseui.auth.AuthUI.getInstance() ||  new firebaseui.auth.AuthUI(firebase.auth());
+
+    ui.start('#firebaseui-auth-container', {
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      signInFlow: 'popup',
+      signInSuccessUrl: 'home.html'
+    });
+}
+
+function removeLogin(){
+    alert("Sucess");
+}
+
+function getUsuario(){
+  firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log("onAuthStateChanged")
+        console.log(user.displayName)
+          return user;
+      }
+  });
+}
