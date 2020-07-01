731b47495def17a56b1b0bf998fb46caafc9fbfd
Ítalo de Queiroz <italo.de.queiroz@avanade.com>
2020-06-30 21:10:45 -0300

    criando método que valida o usuário está logado e criando navbar template

--- public/js/login.js
+++ public/js/login.js
@@ -33,3 +33,13 @@ function getUsuario(){
       }
   });
 }
+
+function isUsuarioLogado(){
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user == null) {
+        window.location.href = "index.html";
+    }
+});
+
+}
